e3fb709193f3e356a1085a7ca583d9875184d2f3
sanjaiveeran44 <kit27.am44@gmail.com>
2025-10-15 10:59:45 +0530

    setup frontend using react

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -12,3 +12,7 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// admin address = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+
+// contract address = 0x5FbDB2315678afecb367f032d93F642f64180aa3
