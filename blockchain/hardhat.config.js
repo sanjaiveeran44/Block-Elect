c9eab733b078fca11d784e633badfd69c9164fc5
sanjaiveeran44 <kit27.am44@gmail.com>
2025-10-17 14:38:45 +0530

    Update hardhat.config.js: configure myLocal network with deployer account from .env

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    myLocal: {
+      url: "http://127.0.0.1:8545",  
+      accounts: [process.env.METAMASK_PRIVATE_KEY]  
+    }
+  }
 };
